54a3e6c2d51cc076c6a6e9bd08293faff4daa2a1
Fabián <fabianveal@gmail.com>
2025-02-09 15:17:02 -0300

    chore(env): remove raw env vars

--- docker/init-database.sh
+++ docker/init-database.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-set -ex
+# Get environment variables from .env file
+POSTGRES_DB=$(grep POSTGRES_DB .env | xargs)
+POSTGRES_USER=$(grep POSTGRES_USER .env | xargs)
+POSTGRES_PASSWORD=$(grep POSTGRES_PASSWORD .env | xargs)
 
+printf "POSTGRES_DB: $POSTGRES_DB\n"
+printf "POSTGRES_USER: $POSTGRES_USER\n"
+
+set -ex
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER docker;
-    CREATE DATABASE tenpo;
-    GRANT ALL PRIVILEGES ON DATABASE tenpo TO docker;
-    CREATE DATABASE tenpo_test;
-    GRANT ALL PRIVILEGES ON DATABASE tenpo_test TO docker;
+    CREATE DATABASE $POSTGRES_DB;
+    GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO docker;
 EOSQL
