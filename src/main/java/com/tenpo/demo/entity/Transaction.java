1f4747b23ea11f9b0094d1e8d585521859ad50f9
Fabián <fabianveal@gmail.com>
2025-02-09 15:17:58 -0300

    style(entity/transaction): format imports

--- src/main/java/com/tenpo/demo/entity/Transaction.java
+++ src/main/java/com/tenpo/demo/entity/Transaction.java
@@ -3,13 +3,8 @@ package com.tenpo.demo.entity;
 import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
-
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.annotation.Priority;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -19,7 +14,6 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-
 
 @Entity
 @Table(name = "transactions")
